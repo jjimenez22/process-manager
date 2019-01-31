1215a6e382606e52aa3c1a7729894ce6aed59dc5
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-01-31 14:51:58 -0400

    User dto and lombok

---
+++ backend/src/main/java/ve/com/jjimenez/persistence/model/UserDTO.java
@@ -0,0 +1,23 @@
+package ve.com.jjimenez.persistence.model;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class UserDTO {
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String matchingPassword;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+}
