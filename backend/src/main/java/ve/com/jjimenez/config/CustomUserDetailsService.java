6d50919bb3ed1d5dfe3e5fc3bdcd1049d40ad51a
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-02-01 12:54:00 -0400

    User details service

---
+++ backend/src/main/java/ve/com/jjimenez/config/CustomUserDetailsService.java
@@ -0,0 +1,48 @@
+package ve.com.jjimenez.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ve.com.jjimenez.persistence.model.user.User;
+import ve.com.jjimenez.persistence.repo.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    private static Collection<GrantedAuthority> getAuthority(String role) {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = repository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("Not found user: " + username)
+        );
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                true, /*enabled*/
+                true, /*account non expired*/
+                true, /*credentials non expired*/
+                true, /*account non locked*/
+                getAuthority(user.getRole())
+        );
+    }
+}
