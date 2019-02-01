a72067036918942cce743d29b1d67ad201899f2e
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-02-01 12:55:03 -0400

    Security configuration for login and filtering requests by role.

--- backend/src/main/java/ve/com/jjimenez/config/SecurityConfig.java
+++ backend/src/main/java/ve/com/jjimenez/config/SecurityConfig.java
@@ -1,18 +1,58 @@
 package ve.com.jjimenez.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final SavedRequestAwareAuthenticationSuccessHandler successHandler =
+            new SavedRequestAwareAuthenticationSuccessHandler();
+    private final SimpleUrlAuthenticationFailureHandler failureHandler = new SimpleUrlAuthenticationFailureHandler();
+
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private final CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(RestAuthenticationEntryPoint restAuthenticationEntryPoint, CustomUserDetailsService userDetailsService) {
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll()
-                .and().csrf().disable();
+        http.csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/user/**").hasRole("ADMIN")
+                .antMatchers("/api/process/**").hasRole("MANAGER")
+                .antMatchers("/api/conclusion/**").hasRole("ENDER")//todo right translation
+                .and()
+                .formLogin()
+                .successHandler(successHandler)
+                .failureHandler(failureHandler)
+                .and()
+                .logout();
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
