14e0c824a8cb9fae18e7f642d70a1b863c9d23ea
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-02-02 00:32:54 -0400

    Revert save hiding

--- backend/src/main/java/ve/com/jjimenez/persistence/repo/UserRepository.java
+++ backend/src/main/java/ve/com/jjimenez/persistence/repo/UserRepository.java
@@ -1,7 +1,6 @@
 package ve.com.jjimenez.persistence.repo;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 import ve.com.jjimenez.persistence.model.user.User;
 
@@ -12,7 +11,7 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
-    @Override
-    @RestResource(exported = false)
-    <S extends User> S save(S s);
+//    @Override
+//    @RestResource(exported = false)
+//    <S extends User> S save(S s);
 }
