71cffa81da23874be19c355dd4fea0c3626b2d3e
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-02-02 15:46:41 -0400

    Allow cors

--- backend/src/main/java/ve/com/jjimenez/persistence/repo/ProcessRepository.java
+++ backend/src/main/java/ve/com/jjimenez/persistence/repo/ProcessRepository.java
@@ -2,8 +2,10 @@ package ve.com.jjimenez.persistence.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import ve.com.jjimenez.persistence.model.Process;
 
 @Repository
+@CrossOrigin
 public interface ProcessRepository extends CrudRepository<Process, Long> {
 }
--- backend/src/main/java/ve/com/jjimenez/persistence/repo/UserRepository.java
+++ backend/src/main/java/ve/com/jjimenez/persistence/repo/UserRepository.java
@@ -2,11 +2,13 @@ package ve.com.jjimenez.persistence.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import ve.com.jjimenez.persistence.model.user.User;
 
 import java.util.Optional;
 
 @Repository
+@CrossOrigin
 public interface UserRepository extends CrudRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
