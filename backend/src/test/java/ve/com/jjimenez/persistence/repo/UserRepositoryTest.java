aae909165532eb106f307b47fbb6ea2a08ddfa5a
Jesus Jimenez <jgjimenezb94@gmail.com>
2019-02-05 21:47:04 -0400

    User repository test fix

--- backend/src/test/java/ve/com/jjimenez/persistence/repo/UserRepositoryTest.java
+++ backend/src/test/java/ve/com/jjimenez/persistence/repo/UserRepositoryTest.java
@@ -3,22 +3,18 @@ package ve.com.jjimenez.persistence.repo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import ve.com.jjimenez.persistence.model.User;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
-class UserRepositoryTest {
-
-    @Autowired
-    EntityManager entityManager;
+@SpringBootTest
+public class UserRepositoryTest {
 
     @Autowired
     UserRepository repository;
@@ -27,9 +23,10 @@ class UserRepositoryTest {
     public void whenFindByUsername_thenReturnUser() {
 //        given
         User user = new User();
-        user.setUsername("test");
-        entityManager.persist(user);
-        entityManager.flush();
+        user.setUsername("whenFindByUsername");
+        user.setPassword("test");
+        user.setRole("role");
+        repository.save(user);
 
 //        when
         Optional<User> found = repository.findByUsername(user.getUsername());
@@ -43,10 +40,10 @@ class UserRepositoryTest {
     public void whenFindAllByRole_thenReturnUser() {
 //        given
         User user = new User();
-        user.setUsername("test");
+        user.setUsername("whenFindAllByRole");
+        user.setPassword("test");
         user.setRole("role");
-        entityManager.persist(user);
-        entityManager.flush();
+        repository.save(user);
 
 //        when
         List<User> found = repository.findAllByRole(user.getRole());
